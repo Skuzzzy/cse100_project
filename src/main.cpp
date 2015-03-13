d31b68e95ef1d7ea623b2b4d0d1a4e3fe289ddfa
Daniel Baird <JamesDanielBaird@gmail.com>
2015-03-12 19:28:48 -0700

    removing commented out code

--- src/main.cpp
+++ src/main.cpp
@@ -9,44 +9,6 @@ using namespace std;
 
 int main()
 {
-    /*
-    card a(3,3);
-    card b(3,13);
-    card c(3,10);
-
-    cout << c.toString() << endl;
-
-    hand h;
-    h.addCard(a);
-
-    h.addCard(b);
-    h.addCard(c);
-
-    cout << h.calculateHandValue() << endl;
-
-    cout << h.toString() << endl;
-
-    hand qwe;
-    cout << "*" << qwe.toString() << "*" << endl;
-
-    qwe.addCard(a);
-
-    cout << "*" << h.toString() << "*" << endl;
-
-    human asd(50);
-
-    cout << asd.getChips();
-
-    asd.addCardToHand(a);
-    asd.addCardToHand(b);
-
-    asd.requestCard();
-
-    deck d;
-
-    cout << d.popCard().toString() << endl;
-    */
-
     game bjg(1,50);
 
     return 0;
